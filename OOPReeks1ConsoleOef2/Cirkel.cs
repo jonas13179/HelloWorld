71648e0772623264da288947a5bffd225d022e94
Gorrens Jonas <jonas13179@leerling.sfc.be>
2026-04-22 08:53:56 +0200

    first commit

---
+++ OOPReeks1ConsoleOef2/Cirkel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPReeks1ConsoleOef2
+{
+    internal class Cirkel
+    {
+        //Velden
+        string _kleur;
+        double _straal;
+
+        //Eigenschappen
+        public string kleur
+        {
+            get { return _kleur; }
+            set { _kleur = value; }
+
+        }
+        public double Straal
+        {
+            get { return _straal; }
+            set { _straal = value; }
+
+        }
+        //methodes
+        public double Omtrek()
+        {
+            return Math.Round(2 * Math.PI * _straal, 2);
+
+        }
+        public double Oppervlakte()
+        {
+            return  Math.Round(Math.PI * Math.Pow(_straal, 2),3);
+
+        }
+        //constructor
+        public Cirkel() //overlaoding
+        {
+            _straal = 10;
+            _kleur = "blauw";
+        }
+        public Cirkel(string kleur, double straal) //overlaoding
+        {
+            _straal = straal;
+            _kleur = kleur;
+
+           
+  
+        }
+    }
+}
---
+++ OOPReeks1ConsoleOef2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+namespace OOPReeks1ConsoleOef2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cirkel standaard = new Cirkel();
+           //variabelen declareren
+            string kleur;
+            double straal;
+            
+            //gerbruiker cirkel
+           
+            Console.WriteLine("wat is de kler van uw cirkel: ");
+            kleur = Console.ReadLine();
+            Console.WriteLine("wat is de straal van uw crikel: ");
+            straal = double.Parse(Console.ReadLine());
+            
+            Cirkel mijnCirkel = new Cirkel(kleur, straal);
+
+            //resultaten gebruiker cirkel
+            Console.WriteLine($"De resultaten van mijn cirkel gekozen cirkel: ");
+            Console.WriteLine($"De omtrek: {mijnCirkel.Omtrek()}");
+            Console.WriteLine($"De oppervlakte: {mijnCirkel.Oppervlakte()}");
+
+            //resultaten standaard cirkel
+            Console.WriteLine($"De resultaten van de standaard cirkel: ");
+            Console.WriteLine($"De omtrek: {standaard.Omtrek()}");
+            Console.WriteLine($"De oppervlakte: {standaard.Oppervlakte()}");
+
+            Console.ReadKey();  
+
+
+        }
+    }
+}
