6d023a315414bca90e8c92ff040343b0577f3b0b
jonas13179 <jonas13179@SPB2023-028469>
2026-04-22 09:35:36 +0200

    txt file aangepast

--- OOPReeks1ConsoleOef2/obj/Release/net8.0/OOPReeks1ConsoleOef2.AssemblyInfo.cs
+++ OOPReeks1ConsoleOef2/obj/Release/net8.0/OOPReeks1ConsoleOef2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPReeks1ConsoleOef2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb9635fbf616fb07a95d693852d344e3f1f89e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPReeks1ConsoleOef2")]
